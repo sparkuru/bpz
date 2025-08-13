3ddd4c780a0d4557277d3d17c88b4f7dcf17b517
sparkle <wkyuu@majo.im>
2025-08-13 16:45:38 +0800

    u1x

--- generator.py
+++ generator.py
@@ -1,10 +1,15 @@
 # -*- coding: utf-8 -*-
 
-CONSTANT = 'github.com#?#div.Box-sc-g0xbh4-0.{}:-abp-has(a:-abp-contains({}))'
-KEYWORD = 'flszRz'
-ruozhi_file_path = 'crap'
+CONSTANT = "github.com#?#div.Box-sc-g0xbh4-0.{}:-abp-has(a:-abp-contains({}))"
+KEYWORDS = [
+    "flszRz",
+    "bmcJak",
+]
+
+shit_path = "crap"
 
 BANLIST = []
-with open(ruozhi_file_path, 'r') as f:
+with open(shit_path, "r") as f:
     BANLIST.extend(f.read().splitlines())
-[ print(CONSTANT.format(KEYWORD, x)) for x in BANLIST ]
+for keyword in KEYWORDS:
+    [print(CONSTANT.format(keyword, x)) for x in BANLIST]
